57d119dfd8f7133bbead94299ee01fc47a9c3f00
nhhung1810 <nghung224@gmail.com>
2023-05-05 22:47:10 +0700

    Fix inference code and loading module

--- scripts/experiments/one_point/inference.py
+++ scripts/experiments/one_point/inference.py
@@ -8,9 +8,10 @@ from tqdm import tqdm
 from scripts.sam_train import SamTrain
 from scripts.datasets.constant import VAL_METADATA, DEFAULT_DEVICE
 from scripts.datasets.flare22_one_point import FileLoader
-from scripts.utils import load_file_npz, load_img, omit
+from scripts.utils import load_file_npz, load_img, make_directory, omit
 from segment_anything.build_sam import sam_model_registry
 from segment_anything.modeling.sam import Sam
+from scripts.losses.loss import DiceLoss
 
 
 def load_model(model_path, device=DEFAULT_DEVICE) -> Sam:
@@ -31,15 +32,20 @@ def make_point_from_mask(mask: Tensor) -> Tuple[Tensor, Tensor]:
 if __name__ == "__main__":
     file_loader = FileLoader(metadata_path=VAL_METADATA)
     patient_data = defaultdict(list)
+    device = DEFAULT_DEVICE
 
     TEST_MASK_PATH = (
         "dataset/FLARE22-version1/FLARE22_LabeledCase50/labels/FLARE22_Tr_0008.nii.gz"
     )
     run_path = "sam-one-point-230501-012024"
-    model_path = f"{run_path}/model-1.pt"
+    model_path = f"runs/{run_path}/model-10.pt"
     model = load_model(model_path)
     sam_train = SamTrain(sam_model=model)
-    inference_save_dir = f"{run_path}/inference/"
+    inference_save_dir = f"runs/{run_path}/inference/"
+
+    # Liver only
+    class_num = 1
+    dice_fn = DiceLoss(activation=None, reduction="none")
 
     for data in file_loader.data:
         patient_data[data["name"]].append(omit(data, ["name"]))
@@ -58,31 +64,43 @@ if __name__ == "__main__":
         ):
             img = load_img(ct_slice["img_path"])
             mask = torch.as_tensor(load_file_npz(ct_slice["mask_path"]))
-            coors, labels = make_point_from_mask(mask=mask)
+            mask = mask == class_num
+            if mask.any():
+                coors, labels = make_point_from_mask(mask=mask == class_num)
+            else:
+                coors, labels = None, None
 
-            img_emb, original_size, input_size = sam_train.prepare_img(image=img)
+            # img_emb
+            with torch.no_grad():
+                img_emb, original_size, input_size = sam_train.prepare_img(image=img)
 
-            coords_torch, labels_torch, _, _ = sam_train.prepare_prompt(
-                original_size=original_size,
-                point_coords=coors,
-                point_labels=labels,
-            )
+                coords_torch, labels_torch, _, _ = sam_train.prepare_prompt(
+                    original_size=original_size,
+                    point_coords=coors,
+                    point_labels=labels,
+                )
 
-            mask_pred, iou_pred, _ = sam_train.predict_torch(
-                image_emb=img_emb,
-                input_size=input_size,
-                original_size=original_size,
-                multimask_output=True,
-                # Eval need no logits
-                return_logits=False,
-                point_coords=coords_torch,
-                point_labels=labels_torch,
-                mask_input=None,
-            )
+                mask_pred, iou_pred, _ = sam_train.predict_torch(
+                    image_emb=img_emb, # 1, 256, 64, 64
+                    input_size=input_size,
+                    original_size=original_size,
+                    multimask_output=True,
+                    # Eval need no logits
+                    return_logits=False,
+                    point_coords=coords_torch,
+                    point_labels=labels_torch,
+                    mask_input=None,
+                )
+                
+                mask = mask.unsqueeze(0).repeat_interleave(3, dim=0).unsqueeze(0).type(torch.int64)
+                mask = mask.to(device=device)
+                dice = dice_fn.run_on_batch(input=mask_pred, target=mask)
+                chosen_idx = dice.argmin()
+                mask_pred = mask_pred[0, chosen_idx]
 
-            all_pred.append(mask_pred.detach().cpu().numpy())
+                all_pred.append(mask_pred.detach().cpu().numpy())
 
-            pass
+                pass
         # Stack all prediction into one
         all_pred = np.stack(all_pred, axis=0)
 
@@ -90,6 +108,7 @@ if __name__ == "__main__":
         # H, W, T
         all_pred = all_pred.transpose(1, 2, 0).astype(np.uint8)
         mask_out_path = f"{inference_save_dir}/{patient_name}.npy"
+        make_directory(mask_out_path, is_file=True)
         np.save(mask_out_path, all_pred)
         pass
 
--- scripts/tools/evaluation/loading.py
+++ scripts/tools/evaluation/loading.py
@@ -1,5 +1,7 @@
+import os
 import SimpleITK as sitk
 import numpy as np
+from tqdm import tqdm
 
 
 def load_ct_info(file_path):
@@ -56,3 +58,51 @@ def save_ct_from_npy(
     if direction is not None:
         sitk_image.SetDirection(direction)
     save_ct_from_sitk(sitk_image, save_path, sitk_type, use_compression)
+
+def postprocess(pred_file, gt_file, out_dir):
+    # filenames = os.listdir(gt_file)
+    os.makedirs(out_dir, exist_ok=True)
+
+    print("Processing prediction files")
+    # for test_filename in tqdm(filenames):
+    raw_image_path = gt_file
+    # pred_test_filename = gt_file.replace('.nii.gz', '.npy')
+    pred_image_path = pred_file
+
+    if not os.path.isfile(pred_image_path):
+        return
+    assert os.path.isfile(pred_image_path), f"Missing {pred_image_path}"
+
+    raw_image_dict = load_ct_info(raw_image_path)
+    pred_image_dict = {
+        'mask': np.load(pred_image_path).transpose(2,0,1)
+    }
+
+    pred_image_dict["mask"] = change_axes_of_image(
+        pred_image_dict["mask"], raw_image_dict["subdirection"]
+    )
+
+    out_test_filename = os.path.basename(gt_file).replace('_0000.nii.gz', '.nii.gz')
+    dest_image_path = os.path.join(out_dir, out_test_filename)
+
+    save_ct_from_npy(
+        npy_image=pred_image_dict["mask"],
+        save_path=dest_image_path,
+        origin=raw_image_dict["origin"],
+        spacing=raw_image_dict["spacing"],
+        direction=raw_image_dict["direction"],
+        sitk_type=sitk.sitkUInt8,
+    )
+
+if __name__ == "__main__":
+    TEST_MASK_PATH = (
+        "dataset/FLARE22-version1/FLARE22_LabeledCase50/labels/FLARE22_Tr_0008.nii.gz"
+    )
+
+    PRED_MASK_PATH = (
+        "runs/sam-one-point-230501-012024/inference/FLARE22_Tr_0008_0000.npy"
+    )
+
+
+    postprocess(pred_file=PRED_MASK_PATH, gt_file=TEST_MASK_PATH, out_dir=os.path.dirname(PRED_MASK_PATH))
+    pass
