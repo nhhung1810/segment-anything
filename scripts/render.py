e2550b1baa50e38b16c8df04972506dabc94208d
nhhung1810 <nghung224@gmail.com>
2023-05-05 14:53:30 +0700

    Visualization code

--- scripts/render.py
+++ scripts/render.py
@@ -1,67 +1,98 @@
 from typing import Dict, List, Tuple
+from matplotlib.axes import Axes
+from matplotlib.lines import Line2D
 import numpy as np
 import matplotlib.pyplot as plt
 
-def overlay_mask(mask, ax, no_overlay=False):
-    if no_overlay:
-        ax.imshow(mask)
-        return
-    color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
+from scripts.datasets.color import get_color
+
 
 class Renderer:
-    def __init__(self) -> None:
+    def __init__(self, legend_dict: Dict[int, object]) -> None:
+        self.legend_dict = legend_dict
         self.data = []
+        pass
+
+    def build_class_name_map(self, masks: np.ndarray):
+        result = {}
+        for class_num in np.unique(masks):
+            try:
+                label = self.legend_dict[class_num]
+            except Exception:
+                label = f"{class_num}"
+            result[class_num] = label
+
+        return result
+
+    def overlay_mask(self, mask, ax: Axes, no_overlay=False):
+        if no_overlay:
+            ax.imshow(mask)
+            return
+
+        h, w = mask.shape[-2:]
+        masks = mask.reshape(h, w)
+
+        result = np.ones([h, w, 4])
+        # * color.reshape(1, 1, -1)
+        handle_legend = []
+        class_label_map = self.build_class_name_map(masks)
+        for class_num in np.unique(masks):
+            (r, b, g, _) = get_color(class_num) if class_num != 0 else (0, 0, 0, 0)
+            color = (r, b, g, 0.6)
+            handle_legend.append(
+                Line2D(
+                    xdata=[0],
+                    ydata=[0],
+                    color=color,
+                    lw=2,
+                    label=f"{class_label_map[class_num]}",
+                )
+            )
+            result[masks == class_num] = color
+            pass
+
+        ax.legend(handles=handle_legend, bbox_to_anchor=(1.4, 1), loc="upper right")
+        ax.imshow(result)
         pass
 
     def add_multiple(self, arrays: List[Dict]):
         for data in arrays:
             self.add(
-                img=data.get('img', None),
-                mask=data.get('mask', None),
-                points=data.get('points', None),
-                title=data.get('title', None),
+                img=data.get("img", None),
+                mask=data.get("mask", None),
+                points=data.get("points", None),
+                title=data.get("title", None),
             )
             pass
         pass
 
-    def add(self, 
-            img: np.ndarray, 
-            mask: np.ndarray,
-            points: Tuple[np.ndarray, np.ndarray], 
-            title: str,
-            ):
-        
-        assert img is not None or mask is not None, \
-            'Both img or mask is None'
-        
+    def add(
+        self,
+        img: np.ndarray,
+        mask: np.ndarray,
+        points: Tuple[np.ndarray, np.ndarray] = None,
+        title: str = "",
+    ):
+        assert img is not None or mask is not None, "Both img or mask is None"
+
         _img = self._format_img(img)
         _mask = self._format_mask(mask)
 
-
         self.data.append(
-            {
-                'img': _img,
-                'mask': _mask,
-                'points': points,
-                'title': title or 'Untitled'
-            }
+            {"img": _img, "mask": _mask, "points": points, "title": title or "Untitled"}
         )
-        
-        pass
+        return self
 
     def _format_mask(self, mask):
-        if mask is None: 
+        if mask is None:
             return None
 
         assert mask.ndim <= 3, f"Mask dim <= 3, while get {mask.shape}"
         # Reshape mask to dim of 2
         if mask.ndim == 3:
-            if mask.shape[0] == 1: 
+            if mask.shape[0] == 1:
                 _mask = mask[0]
-            elif mask.shape[-1] == 1: 
+            elif mask.shape[-1] == 1:
                 _mask = mask[:, :, 0]
         else:
             _mask = mask.copy()
@@ -69,7 +100,8 @@ class Renderer:
         return _mask
 
     def _format_img(self, img):
-        if img is None: return None
+        if img is None:
+            return None
 
         # Check for img data
         assert img.ndim < 4, "Out of control"
@@ -78,58 +110,63 @@ class Renderer:
             _img = _img[:, :, None]
 
         if img.ndim == 3:
-            assert _img.shape[-1] == 1 or _img.shape[-1] == 3, \
-                f"Invalid shape {img.shape} transform to {_img.shape}"
+            assert (
+                _img.shape[-1] == 1 or _img.shape[-1] == 3
+            ), f"Invalid shape {img.shape} transform to {_img.shape}"
             pass
         return _img
 
-    def show_all(self, save_path: str=None):
+    def show_all(self, save_path: str = None):
         assert len(self.data) > 0, "There is no data to be rendered"
         f, ax, n_row, n_col = self._get_valid_subplot(len(self.data))
 
         for i1 in range(n_row):
             for i2 in range(n_col):
                 idx = i1 * n_col + i2
-                if idx >= len(self.data): break
+                if idx >= len(self.data):
+                    break
 
                 _data = self.data[idx]
-                if _data['img'] is not None:
-                    ax[i1, i2].imshow(_data['img'])
+                if _data["img"] is not None:
+                    ax[i1, i2].imshow(_data["img"])
                     pass
 
-                if _data['mask'] is not None:
-                    overlay_mask(
-                        _data['mask'], 
-                        ax[i1, i2], 
-                        no_overlay=_data['img'] is None
-                        )
+                if _data["mask"] is not None:
+                    self.overlay_mask(
+                        _data["mask"], ax[i1, i2], no_overlay=_data["img"] is None
+                    )
                     pass
 
-                if _data['points'] is not None:
-                    [pcoors, plabels] = _data['points']
+                if _data["points"] is not None:
+                    [pcoors, plabels] = _data["points"]
                     self._render_point_label(
-                        ax[i1, i2], pcoors, plabels, chosen_value=1, color_str='g')
+                        ax[i1, i2], pcoors, plabels, chosen_value=1, color_str="g"
+                    )
                     self._render_point_label(
-                        ax[i1, i2], pcoors, plabels, chosen_value=0, color_str='r')
+                        ax[i1, i2], pcoors, plabels, chosen_value=0, color_str="r"
+                    )
                     pass
 
-                ax[i1, i2].set_title(_data['title'])
+                ax[i1, i2].set_title(_data["title"])
                 pass
         pass
 
         if save_path:
-            f.savefig(save_path)
+            f.savefig(
+                save_path,
+                # bbox_inches="tight"
+            )
             plt.close()
-            return
+            return self
         else:
             f.show()
-        pass
-        
 
-    def _render_point_label(self, ax, pcoors, plabels, chosen_value=1, color_str='g'):
-        if pcoors is None: 
+        return self
+
+    def _render_point_label(self, ax, pcoors, plabels, chosen_value=1, color_str="g"):
+        if pcoors is None:
             return
-        if plabels is None: 
+        if plabels is None:
             return
         msk = plabels == chosen_value
         if msk.any():
@@ -137,17 +174,20 @@ class Renderer:
             ys = pcoors[msk][:, 1]
             ax.scatter(xs, ys, c=color_str)
 
-
     def _get_valid_subplot(self, n):
+        if n == 1:
+            f, axes = plt.subplots(1, 1, squeeze=False)
+            return f, axes, 1, 1
+
         if n <= 3:
             n_row, n_col = 1, n
-        
+
         n_row = int(np.floor(np.sqrt(n)))
         n_row, n_col = n_row, n_row + 1
         f, axes = plt.subplots(n_row, n_col, squeeze=False)
 
         return f, axes, n_row, n_col
-    
+
     def reset(self):
         self.data.clear()
         plt.close()
