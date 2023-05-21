62b13c50516a30c63b55f43fbf030d549256fd35
nhhung1810 <nghung224@gmail.com>
2023-05-21 22:23:09 +0700

    Sevaral change to the run script

--- bash_scripts/multiple_train_masks_aug.sh
+++ bash_scripts/multiple_train_masks_aug.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Raw intitialized, try to overfit
-python scripts/experiments/mask_aug/train.py with \
-   learning_rate=6e-4 \
-   n_epochs=300 \
-   evaluate_epoch=10 \
-   save_epoch=10
+# python scripts/experiments/mask_aug/train.py with \
+#    learning_rate=6e-4 \
+#    n_epochs=300 \
+#    evaluate_epoch=10 \
+#    save_epoch=10
 
 # First mask-prop trained with lr=6e-5, 
 # class-focus init inside the train code
 python scripts/experiments/mask_aug/train.py with \
-    learning_rate=6e-5 \
+    learning_rate=6e-6 \
     custom_model_path="runs/mask-aug-230520-140411/model-300.pt" \
     n_epochs=300 \
     evaluate_epoch=10 \
--- bash_scripts/run_submission_mask_aug.sh
+++ bash_scripts/run_submission_mask_aug.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 python bash_scripts/run_submission_mask_aug.py \
-    --model_dir "runs/mask-aug-230520-140411" \
+    --model_dir "runs/mask-aug-230521-004441" \
     --is_custom_class 1 \
     --prefix "maug_19" \
-    --limit 8
+    --skip_eval 1 \
+    --limit 5 \
+
+python bash_scripts/run_submission_mask_aug.py \
+    --model_dir "runs/mask-aug-230521-035709" \
+    --is_custom_class 1 \
+    --prefix "maug_19" \
+    --skip_eval 1 \
+    --limit 5 \
 
 # python bash_scripts/run_submission_mask_aug.py \
 #     --model_dir "runs/mask-aug-230520-140411" \
